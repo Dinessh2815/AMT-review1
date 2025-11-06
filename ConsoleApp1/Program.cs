4279da31d2c48ca13634d424e746511e85eb1452
Dinessh2815 <dinessh.venkat28@gmail.com>
2025-11-06 12:53:52 +0530

    Count number of prime completed

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace ConsoleApp1
+﻿using System;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] arr = { 2, 4, 5, 7, 9, 11 };
+            int count = 0;
+            bool isPrime = false;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                
+                for (int j = 2; j < i; j++)
+                {
+                    if (arr[i] != arr[j])
+                    {
+                        if (arr[i] % arr[j] != 0)
+                        {
+                            isPrime = true;
+
+                        }
+                        else
+                        {
+                            isPrime = false;
+                            break;
+
+
+                        }
+                        if (isPrime)
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+              Console.WriteLine($"The number of prime numbers are : {count}");
         }
     }
 }
--- ConsoleApp1/obj/Release/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Release/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c076f995d0a361b9b5f57075ab26e32962f1906d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
