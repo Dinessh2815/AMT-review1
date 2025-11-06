6ac6efa02e6bb18732dfe1616ae013149a1401d7
Dinessh2815 <dinessh.venkat28@gmail.com>
2025-11-06 13:42:04 +0530

    2.  Subarray with Maximum Sum

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -5,20 +5,25 @@
         static void Main(string[] args)
         {
             int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4 };
-            int first = 0;
-            int second = 0;
             int currentSum = 0;
-            int maxSum = 0;
+            int maxSum = int.MinValue;
 
-            while(second < arr.Length)
+            for (int i = 0; i < arr.Length; i++)
             {
-                currentSum = arr[second];
+                currentSum += arr[i];
 
-                if(currentSum > maxSum)
+                if (currentSum > maxSum)
                 {
                     maxSum = currentSum;
                 }
+
+                if (currentSum < 0)
+                {
+                    currentSum = 0;
+                }
             }
+
+            Console.Write(maxSum);
         }
     }
 }
