5d40adc9b580895c88989660dafeb8b3a35ddb0d
Dinessh2815 <dinessh.venkat28@gmail.com>
2025-11-06 13:05:53 +0530

    2nd problem pending

--- ConsoleApp1/obj/Release/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Release/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c076f995d0a361b9b5f57075ab26e32962f1906d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4279da31d2c48ca13634d424e746511e85eb1452")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4 };
+            int first = 0;
+            int second = 0;
+            int currentSum = 0;
+            int maxSum = 0;
+
+            while(second < arr.Length)
+            {
+                currentSum = arr[second];
+
+                if(currentSum > maxSum)
+                {
+                    maxSum = currentSum;
+                }
+            }
+        }
+    }
+}
